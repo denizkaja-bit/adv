9cbed013ea868b724779b6ca5588b17f76cdd0fc
Denko <denizkaja@gmail.com>
2026-05-15 11:33:50 +0300

    Add files via upload

---
+++ den/guzergah.php
@@ -0,0 +1,69 @@
+<?php
+include "db.php";
+//Silme işlemi
+if (isset($_GET["sil"])) {
+    $id = $_GET["sil"]; //Otobüs id'si
+
+    //Bu güzergaha bağlı otobüs var mı?
+    $kontrolSQL = "SELECT * from otobusler WHERE guzergah_id=$id";
+    $kontrolSonuc = mysqli_query($baglanti, $kontrolSQL);
+    if (mysqli_num_rows($kontrolSonuc) > 0) {
+
+        echo "<script>alert('Bu güzergah başka bir otobüs tarafından kullanılıyor.')</script>";
+
+    } else {
+        $silSQL = "DELETE FROM guzergah WHERE guzergah_id=$id";
+
+        mysqli_query($baglanti, $silSQL);
+        
+        header("Location:guzergah.php");
+        exit;
+    }
+}
+$sql = "SELECT * FROM guzergah";
+//otobusler tablosundaki guzergah_id ile guzergah tablosundaki guzergah_id aynı olanları birleştir
+$sonuc = mysqli_query($baglanti, $sql);
+?>
+
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OTOBÜS YÖNETİM SİSTEMİ</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php include("header.php");?>
+    <section class="ana">
+        <h3>GÜZERGAH BİLGİLERİ</h3>
+        <table>
+            <tr>
+                <th>Sıra No</th>
+                <th>Başlangıç</th>
+                <th>Bitiş</th>
+                <th>Tahmini Süre</th>
+                <th>İşlemler</th>
+            </tr>
+            <?php $sira = 0 ?>
+            <?php while ($dizi = mysqli_fetch_assoc($sonuc)) {
+                $sira++ ?>
+                <tr class="guzergahtr">
+                    <td><?php echo $sira ?></td>
+                    <td><?php echo $dizi["baslangic"]; ?></td>
+                    <td><?php echo $dizi["bitis"]; ?></td>
+                    <td><?php echo $dizi["sure"] . "dk"; ?></td>
+                    <td>
+                        <a href="">🖋️</a>
+                        <a href="guzergah.php?sil=<?php echo $dizi["guzergah_id"] ?>"
+                            onclick="return confirm('Bu güzergah verisini silmek istediğinize eminmisiniz?')">🗑️</a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+    </section>
+</body>
+
+</html>
